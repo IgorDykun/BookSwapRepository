c3b14ebf06be7a88669b984174df33acb1b44345
IgorDykun <dykun.ihor@chnu.edu.ua>
2026-03-16 20:00:54 +0200

    Test existing models from ExternalDataController

--- BookSwap.Tests/UnitTest1.cs
+++ BookSwap.Tests/UnitTest1.cs
@@ -1,10 +1,36 @@
-﻿namespace BookSwap.Tests;
+﻿using BookSwap.Aggregator.Controllers;
+using Xunit;
 
-public class UnitTest1
+namespace BookSwap.Tests
 {
-    [Fact]
-    public void Test1()
+    public class AggregatorDataTests
     {
+        [Fact]
+        public void UserProfileUpdate_ShouldStoreDataCorrectly()
+        {
+            var update = new ExternalDataController.UserProfileUpdate
+            {
+                UserId = 123456789,
+                FavoriteCity = "Київ",
+                Name = "Олексій"
+            };
 
+            Assert.Equal(123456789, update.UserId);
+            Assert.Equal("Київ", update.FavoriteCity);
+            Assert.Equal("Олексій", update.Name);
+        }
+
+        [Fact]
+        public void UserRequest_ShouldHandleEmptyMessage()
+        {
+            var request = new ExternalDataController.UserRequest
+            {
+                UserId = 987,
+                Message = ""
+            };
+
+            Assert.NotNull(request);
+            Assert.Equal("", request.Message);
+        }
     }
 }
